142f638821de5eae29921b07f2c42c96d93b57e2
Bernie January Jr <bernie@myheartsleeve.com>
2022-02-16 20:55:29 +0000

    website and portfolio update

--- projects/scratch-pad/day-1/stringy.js
+++ projects/scratch-pad/day-1/stringy.js
@@ -177,10 +177,10 @@ function sortAscending(stringOne, stringTwo) {
     // YOUR CODE BELOW HERE //
     // if stringOne is greater than stringTwo return -1
     if (stringOne > stringTwo) {
-        return -1;
+        return 1;
     // if stringOne is less than stringTwo return 1
     } else if (stringOne < stringTwo) {
-        return 1;
+        return -1;
     // else return 0 if stringOne is not less than or greater than stringTwo
     } else {
         return 0;
@@ -200,10 +200,10 @@ function sortDescending(stringOne, stringTwo) {
     // YOUR CODE BELOW HERE //
     // if stringOne is less than stringTwo return -1
     if (stringOne < stringTwo) {
-        return -1;
+        return 1;
     // if stringOne is greater than stringTwo return 1  
     } else if (stringOne > stringTwo) {
-        return 1;
+        return -1;
     // else stringOne is = to stringTwo return 0
     } else {
         return 0;
