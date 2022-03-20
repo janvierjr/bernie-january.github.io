d65f6519c627cdd285bce17f7c993adea4dee7db
Bernie January Jr <bernie@myheartsleeve.com>
2022-03-20 21:03:39 +0000

    recursion complete

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -525,16 +525,18 @@ var alternateSign = function(array, output = []) {
     return output;
   }
   // recursion
-  if (Math.abs(array[0]) / array[0] === array[1] / array[1]) {
-    output.push((array[0]));
-    return alternateSign(array.slice(1), output);
+  if (array[0] === Math.abs(array[0])) {
+    output.push(array[0]);
+    output.push(Math.abs(array[1]) * -1);
+    return alternateSign(array.slice(2), output);
   }
-  if (Math.abs(array[0]) !== array[0]) {
-    output.push(array[0] * -1);
-    return alternateSign(array.slice(1), output);
+  if (array[0] !== Math.abs(array[0])) {
+    output.push(Math.abs(array[0]));
+    output.push(Math.abs(array[1]) * -1);
+    return alternateSign(array.slice(2), output);
   }
-};
-
+ 
+}
 // 35. Given a string, return a string with digits converted to their word equivalent.
 // Assume all numbers are single digits (less than 10).
 // numToText("I have 5 dogs and 6 ponies"); // "I have five dogs and six ponies"
